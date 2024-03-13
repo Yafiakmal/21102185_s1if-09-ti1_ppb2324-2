32798e84bf5876eef6a3d43e000532f2a3c5f7b7
yafi <yafiakmal45@gmail.com>
2024-03-13 11:27:28 +0700

    menambahkan app bar yang berisi icon, column pertama, column kedua

--- praktikum_4/lib/main.dart
+++ praktikum_4/lib/main.dart
@@ -9,10 +9,88 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    
-       return MaterialApp( debugShowCheckedModeBanner: false, title: 'Praktikum 4', home: Scaffold( appBar: AppBar(), body: ListView(), ) ); 
-    
+  Widget build(BuildContext context) {    
+      return MaterialApp( 
+        debugShowCheckedModeBanner: false, 
+        title: 'Praktikum 4',
+        home: Scaffold( 
+            appBar: AppBar( 
+              leading: Icon(Icons.arrow_back), 
+              backgroundColor: Colors.red, ), 
+               body: ListView(
+                children: [
+                  Container( 
+                    height: 250, 
+                    color: Colors.red, 
+                    child: Column( 
+                      mainAxisAlignment: MainAxisAlignment.center, 
+                      children: [ //image 
+                      Container( 
+                        padding: EdgeInsets.all(20) ,
+                        decoration: BoxDecoration( 
+                          shape: BoxShape.circle, 
+                          color: Colors.lightBlueAccent 
+                          ), 
+                          child: Icon(
+                            Icons.person, 
+                            size: 30,
+                            ), 
+                            ), 
+                            SizedBox(
+                              height: 15,
+                              ), //Nama 
+                              Text (
+                                "Puspita Kartika Sari", 
+                                style: TextStyle( 
+                                  color: Colors.white, 
+                                  fontWeight: FontWeight.bold 
+                                  ),
+                                  ), 
+                                  SizedBox(height: 5,), 
+                                  Text (
+                                    "2010xxxxxx",
+                                    style: TextStyle( 
+                                      color: Colors.white, 
+                                      fontWeight: FontWeight.bold 
+                                      ),
+                                      ), 
+                                      SizedBox(height: 5,), 
+                                      Text (
+                                        "Mahasiswa",
+                                        style: TextStyle( 
+                                          color: Colors.white, 
+                                          ),) 
+                                        ], 
+                                      ), 
+                                    ), 
+                                     Container( 
+                                      margin: EdgeInsets.all(30), 
+                                      padding: EdgeInsets.all(20), 
+                                      decoration: BoxDecoration( 
+                                        borderRadius: BorderRadius.circular(10), 
+                                        color: Color(0xffFEF4F3) ), 
+                                        child: Column( 
+                                          crossAxisAlignment: CrossAxisAlignment.start, 
+                                          children: [ 
+                                            Text("Data Diri", style: TextStyle( 
+                                              color: Colors.pinkAccent, 
+                                              fontWeight: FontWeight.bold 
+                                              ),
+                                            ), 
+                                            SizedBox(height: 15,), 
+                                            Row( 
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+                                              children: [ 
+                                                Text("Nama "), 
+                                                Text("Puspita") 
+                                              ], 
+                                            ),
+
+Row( mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [ Text("Kelas "), Text("TI 1") ], ), SizedBox(height: 15,), Row( mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [ Text("Program Studi"), Text("Teknik Informatika") ], ), SizedBox(height: 15,), Row( mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [ Text("Dosen Wali "), Text("NAP") ], ), SizedBox(height: 15,), Row( mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [ Text("Angkatan "), Text("2021") ], ) ], ), ) 
+                                    ], 
+                                  ), 
+          ) 
+          ); 
   }
 }
 
