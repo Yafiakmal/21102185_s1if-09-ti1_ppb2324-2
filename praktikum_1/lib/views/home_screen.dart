f652def9adad0382270f2989f0c45c6bdc5e9a35
yafi <yafiakmal45@gmail.com>
2024-03-15 21:20:55 +0700

    menambahkan kodingan sesuai modul, kemudian melakukan build apk

---
+++ praktikum_1/lib/views/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum_1/views/add_contact_screen.dart'; 
+
+class HomeScreen extends StatelessWidget { 
+  const HomeScreen({Key? key}) : super(key: key); 
+  
+  @override Widget build(BuildContext context) { 
+    return Scaffold( 
+      appBar: AppBar( 
+        title: const Text('Home'), 
+      ), 
+      floatingActionButton: FloatingActionButton( 
+        onPressed:(){
+           Navigator.push( 
+            context, 
+            MaterialPageRoute( 
+              builder: (context) => const AddContactScreen())); 
+        }, 
+        child: const Icon(Icons.add), 
+      ), 
+    ); 
+  } 
+} 
