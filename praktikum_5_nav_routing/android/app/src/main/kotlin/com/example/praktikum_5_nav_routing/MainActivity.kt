59e5f972903750a345a84ae63dd259d1c181c6ac
yafi <yafiakmal45@gmail.com>
2024-03-20 11:03:43 +0700

    membuat project praktikum_5, navigasi dan routing

---
+++ praktikum_5_nav_routing/android/app/src/main/kotlin/com/example/praktikum_5_nav_routing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.praktikum_5_nav_routing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ praktikum_5_nav_routing/lib/homepage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'seconpage.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('21102185-Muhammad Yafi Akmal'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SecondPage(
+                      data: 'Data dari Home Page',
+                    ),
+                  ),
+                );
+              },
+              child: const Text('Menuju halaman kedua'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/second_page',
+                    arguments: 'Menggunakan route');
+              },
+              child: const Text('Menuju halaman kedua dengan route'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
